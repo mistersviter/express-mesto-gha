bc399f1c4c28ac01d25700dc308ee808cdb99602
Tarabrin Evgeniy <mistersviter@gmail.com>
2022-08-18 19:45:20 +0400

    fix: fixes after second review

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+const { ERROR_NOT_FOUND } = require('./utils/constants');
+
 const { PORT = 3000 } = process.env;
 
 const app = express();
@@ -24,7 +26,7 @@ app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
 app.use((req, res) => {
-  res.status(404).send({ message: 'Ой, такого пути не существует' });
+  res.status(ERROR_NOT_FOUND).send({ message: 'Ой, такого пути не существует' });
 });
 
 app.listen(PORT);
